82d9225f0e562d287b6c47aea901f54c52c4a998
shikhasingh-25 <65523027+shikhasingh-25@users.noreply.github.com>
2021-06-27 14:13:08 +0530

    Add files via upload

---
+++ common_widgets/form_submit_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sketch_2_image/common_widgets/custom_raised_button.dart';
+
+
+class FormSubmitButton extends CustomRaisedButton {
+  FormSubmitButton({
+    @required String text,
+    VoidCallback onPressed,
+  }) : super(
+    child: Text(
+      text,
+      style: TextStyle(color: Colors.white, fontSize: 12.0),
+    ),
+    height: 44.0,
+    color: Colors.indigo,
+    borderRadius: 4.0,
+    onPressed: onPressed,
+  );
+}
